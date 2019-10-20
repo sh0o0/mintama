3ba07a67c97b41c8babd82a3cf2736f31312f0dd
nakar0 <shoutaro425@gmail.com>
2019-10-20 20:33:12 +0900

    none

---
+++ src/router/views/Description.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>ホーム</h1>
+    <v-divider></v-divider>
+    <h2>機能の紹介</h2>
+    <main>
+      <h2>ノート</h2>
+      <section>
+
+        <h3>機能</h3>
+        <div></div>
+        
+        <h3>工夫したところ</h3>
+        <div></div>
+        
+        <h3>難しかったところ・苦労したこと</h3>
+        <div></div>
+
+        <h3>感想</h3>
+        <div></div>
+
+      </section>
+    </main>
+  </div>
+</template>
+<script>
+</script>
+<style scoped lang="sass">
+$main-color: #99FFFF
+$sub-color: #FFCC66
+
+.section-link
+  display: inline-block
+  width: 100%
+  height: 100%
+</style>
